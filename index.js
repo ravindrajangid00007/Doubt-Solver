ab825305b1148dffc2ab8693d210ae1028a10204
ravindrajangid <ravindrajangid00007@gmail.com>
2021-09-02 15:19:51 +0530

    directory structure setuped , server created

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const port = 8000;
+const app = express();
+
+
+
+
+
+app.listen(port, (err) => {
+    if (err) {
+        console.log(`Error: ${err}`);
+        return;
+    }
+    console.log(`Server is running on Port: ${port}`);
+})
